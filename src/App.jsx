7942203e17d57fe00786025d4fc508cae0aec60f
arnab-zero <darnabhere@gmail.com>
2025-01-07 04:56:49 +0600

    Project created and workflow defined

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="font-semibold text-3xl py-5">
+        A Simple React App Ready For Deployment
+      </div>
+      <div className="card">
+        <div className="flex justify-center">
+          <button
+            className="border-blue-700 bg-blue-200 font-medium max-w-fit px-4 py-2 rounded-lg mb-2"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Count is {count}
+          </button>
+        </div>
+        <div>
+          <button
+            className="text-lg font-light underline hover:text-gray-400"
+            onClick={() => setCount(0)}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
